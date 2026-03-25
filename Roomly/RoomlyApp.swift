97e1e96ab1895db4d6f3d044b501521127d294a7
Ethan Thiery <thryethan@gmail.com>
2026-03-25 21:55:03 +0100

    Initial Commit

---
+++ Roomly/RoomlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoomlyApp.swift
+//  Roomly
+//
+//  Created by Ethan on 25/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct RoomlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
